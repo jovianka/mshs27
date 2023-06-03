2c0bfe0312b75d0a619e21f8c610c6add4406158
jovianka <jovianpratama123@gmail.com>
2023-06-03 17:38:14 +0700

    perfect 3d model

--- components/GraduateCap.vue
+++ components/GraduateCap.vue
@@ -1,12 +1,12 @@
 <template>
-  <canvas ref="graduationCapCanvas" class="border"/>
+<canvas ref="graduationCapCanvas" class="border" />
 </template>
 
 <script setup>
 
-import  { Scene, WebGLRenderer, PerspectiveCamera, AmbientLight, AxesHelper, DirectionalLight, DirectionalLightHelper, CameraHelper } from 'three'
+import  { Scene, WebGLRenderer, PerspectiveCamera, AmbientLight, DirectionalLight, PCFSoftShadowMap, } from 'three'
 import  { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import  { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// import  { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const graduationCapCanvas= ref(null)
 let renderer // For setRenderer()
@@ -17,7 +17,7 @@ let graduationCapYIncrement = 0
 const scene = new Scene()
 const camera = new PerspectiveCamera(75, 1, 0.1, 1000)
 
-camera.position.setZ(2.9)
+camera.position.setZ(3)
 
 // Load GLTF model
 const gltfLoader = new GLTFLoader()
@@ -25,48 +25,57 @@ const gltfLoader = new GLTFLoader()
 gltfLoader.load("/graduation-cap/scene.gltf", (gltf) => {
   graduationCapModel = gltf.scene
   scene.add(graduationCapModel)
-  graduationCapModel.castShadow = true
-  graduationCapModel.receiveShadow = true
   graduationCapModel.rotation.z = -0.2
   graduationCapModel.rotation.x =  0.17
   graduationCapModel.rotation.y = -2.4
-  graduationCapModel.position.y = -0.4
-})
+  graduationCapModel.position.y = -0.36
 
+  // Shadows
+  graduationCapModel.traverse((child) => {
+   if (child.isMesh) {
+     child.castShadow = true
+     child.receiveShadow = true
+    }
+  });
+})
 
 // Lighting
 // const pointLight = new PointLight(0xffffff)
 // pointLight.position.set(0, 5, 3)
 // scene.add(pointLight)
 
-const directionalLight = new DirectionalLight(0xffffff)
-directionalLight.position.set(0, 5, 3)
+const directionalLight = new DirectionalLight(0xfcebcf, 1)
+directionalLight.position.set(-20, 20, 25)
 directionalLight.target.position.set(0, 0, 0)
 directionalLight.castShadow = true
 scene.add(directionalLight)
 scene.add(directionalLight.target)
 
-// const ambientLight = new AmbientLight(0x404040)
-// scene.add(ambientLight)
+const ambientLight = new AmbientLight(0x848484)
+scene.add(ambientLight)
 
 // Helpers
 // const pointLightHelper = new PointLightHelper(pointLight)
 // scene.add(pointLightHelper)
 
-const directionalLightHelper = new DirectionalLightHelper(directionalLight)
-scene.add(directionalLightHelper)
+// const directionalLightHelper = new DirectionalLightHelper(directionalLight)
+// scene.add(directionalLightHelper)
 
-const axesHelper = new AxesHelper(10);
-scene.add(axesHelper);
+// const axesHelper = new AxesHelper(10);
+// scene.add(axesHelper);
+
+// const shadowHelper = new CameraHelper(directionalLight.shadow.camera)
+// scene.add(shadowHelper)
 
 // Functions
 function setRenderer() {
   if (graduationCapCanvas.value) {
-    renderer = new WebGLRenderer({ canvas: graduationCapCanvas.value })
+    renderer = new WebGLRenderer({ canvas: graduationCapCanvas.value, alpha: true })
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(500, 500)
     renderer.shadowMap.enabled = true
-    const controls = new OrbitControls(camera, renderer.domElement)
+    renderer.shadowMap.type = PCFSoftShadowMap
+    // const controls = new OrbitControls(camera, renderer.domElement)
   }
 }
 
--- pages/home-proto.vue
+++ pages/home-proto.vue
@@ -1,7 +1,17 @@
 <template>
-  <ClientOnly>
-    <GraduateCap/>
-  </ClientOnly>
+  <div class="relative min-h-screen
+  flex flex-row justify-center
+  bg-cover bg-center bg-[url('https://ik.imagekit.io/c8l1fmjugx/pexels-olia-danilevich-8093032.jpg')]
+  overflow-hidden">
+  <div class="absolute bg-black/50 backdrop-blur-[5px] z-0 w-full h-full"></div>
+
+  <div class="absolute">
+    <ClientOnly>
+      <GraduateCap/>
+    </ClientOnly>
+  </div>
+
+  </div>
 </template>
 
 <script setup>
