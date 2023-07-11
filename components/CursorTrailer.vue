ad2bfd36f36774ed00e509ec9fc1ea4a46e9e403
jovianka <jovianpratama123@gmail.com>
2023-07-11 10:05:59 +0800

    updated cursor trailers to change opacity according to the mouseevent target its hovering on

--- components/CursorTrailer.vue
+++ components/CursorTrailer.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-if="isDesktop" ref="cursorTrailers">
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-50 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-40 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-30 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-20 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-10 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed z-0 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed -z-10 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed -z-20 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed -z-30 pointer-events-none"></div>
-    <div class="cursorTrailer rounded-full bg-gradient-to-bl from-orange-400 to-white w-9 h-9 fixed -z-40 pointer-events-none"></div>
+  <div v-if="isDesktop" ref="cursorTrailersWrapper">
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1010] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1009] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1008] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1007] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1006] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1005] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1004] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1003] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1002] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
+    <div class="cursorTrailer rounded-full bg-white w-9 h-9 fixed z-[1001] pointer-events-none shadow-[gold_0_0_10px_2px]"></div>
   </div>
 </template>
 
@@ -17,43 +17,50 @@
   const { isDesktop } = useDevice()
   let mouseX = ref(0)
   let mouseY = ref(0)
-  const cursorTrailers = ref<HTMLDivElement | null>(null)
+  const cursorTrailersWrapper = ref<HTMLDivElement | null>(null)
   
-  const moveCursorTrailers = () => {
-    if (cursorTrailers.value) {
-      cursorTrailers.value.querySelectorAll(".cursorTrailer").forEach((cursorTrailer, arrIndex) => {
+  const animateCursorTrailers = (e?: MouseEvent) => {
+    if (cursorTrailersWrapper.value) {
+      const cursorTrailers = cursorTrailersWrapper.value.querySelectorAll(".cursorTrailer")
+      const mouseTarget = e?.target as HTMLElement
+
+      cursorTrailers.forEach((cursorTrailer, arrIndex) => {
         let trailingCursorDelay = {
           left: `${mouseX.value}px`,
           top: `${mouseY.value}px`,
-          transform: `scale(${(10 - arrIndex) / 10})`
+          transform: `scale(${(10 - arrIndex) / 10})`,
+          opacity: `${mouseTarget.closest("a, .swiper-button-next, .swiper-button-prev") !== null ? 0.2: 1}`
         }
         
         cursorTrailer.animate(trailingCursorDelay, {
           duration: arrIndex * 15,
           fill: "forwards"
         })
-      });
+      })
+      // if (interacting) {
+      //   cursorTrailers[0].animate({opacity: 50}, {duration: 300})
+      // }
     }
   }
-  
+
   const updateCursorCoord = (e: MouseEvent) => {
-    if (cursorTrailers.value) {
-      cursorTrailers.value.querySelectorAll(".cursorTrailer").forEach((cursorTrailer, arrIndex) => {
+    if (cursorTrailersWrapper.value) {
+      cursorTrailersWrapper.value.querySelectorAll(".cursorTrailer").forEach((cursorTrailer, arrIndex) => {
         mouseX.value = e.clientX - cursorTrailer.clientWidth / 2
         mouseY.value = e.clientY - cursorTrailer.clientHeight / 2
       });
     }
-    moveCursorTrailers()
+    animateCursorTrailers(e)
   }
 
   const updateTouchCoord = (e: TouchEvent) => {
-    if (cursorTrailers.value) {
-      cursorTrailers.value.querySelectorAll(".cursorTrailer").forEach((cursorTrailer, arrIndex) => {
+    if (cursorTrailersWrapper.value) {
+      cursorTrailersWrapper.value.querySelectorAll(".cursorTrailer").forEach((cursorTrailer, arrIndex) => {
         mouseX.value = e.touches[0].clientX - cursorTrailer.clientWidth / 2
         mouseY.value = e.touches[0].clientY - cursorTrailer.clientHeight / 2
       });
     }
-    moveCursorTrailers()
+    animateCursorTrailers()
   }
 
   onMounted(() => {
